8cff7f937b2e1b540405c95b944b125a24339347
Smurf2409 <sarahelna1994@gmail.com>
2023-04-26 23:38:04 +0200

    css in js geändert

--- JS_Lev1_3/assets/Javascript/main.js
+++ JS_Lev1_3/assets/Javascript/main.js
@@ -3,4 +3,5 @@ const gallery = document.getElementById("gallery");
 gallery.innerHTML +="<figure><img src=../assets/img/sonne.jpg style=max-width:70%><figcaption>Fig.1</figcaption></figure>";
 gallery.innerHTML +="<figure><img src=../assets/img/sonne.jpg style=max-width:70%><figcaption>Fig.2</figcaption></figure>";
 gallery.innerHTML +="<figure><img src=../assets/img/sonne.jpg style=max-width:70%><figcaption>Fig.3</figcaption></figure>";
+gallery.style.display="flex";
 
